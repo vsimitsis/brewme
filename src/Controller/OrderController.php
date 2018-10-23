4c322433dc971b80602683c3496a6b478be18271
Vagelis Simitsis <vagelis.simitsis@buildempire.co.uk>
2018-10-23 13:01:42 +0100

    OrderController amends

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -4,14 +4,29 @@ namespace BrewMe\Controller;
 
 class OrderController {
 
+    /**
+     * The available commands
+     */
+    const commands = [
+        'prepare',
+        'grab',
+        'set',
+        'help'
+    ];
+
+    /**
+     * The user arguments
+     *
+     * @var array
+     */
+    protected $args = [];
+
     public function post()
     {
-        //Grab the post data
-        $post = htmlspecialchars($_POST['text'], ENT_QUOTES);
+        //Validate the post and grab the arguments
+        $this->validateInput();
 
-        $order = explode(" ", $post);
-
-        switch ($order[0]) {
+        switch ($this->args[0]) {
             case '-help':
                 return $this->getHelp();
                 break;
@@ -26,6 +41,27 @@ class OrderController {
     private function storeOrder()
     {
         return "Order";
+    }
+
+    /**
+     * Validate the user's input and store the args
+     */
+    private function validateInput()
+    {
+        //Grab the post data
+        $post = htmlspecialchars($_POST['text'], ENT_QUOTES);
+
+        //Seperate arguments and make sure the command is an available command
+        $this->args = explode(" ", $post);
+
+        if (!isset($this->args[0])) {
+            $msg = 'Welcome to BrewMe. Have a look at `brew help` for valid commands';
+            return $this->error($msg);
+        }
+        else if (!in_array($this->args[0], $this->commands)) {
+            $msg = $this.args[0] . ' is not a valid command. Have a look at `brew help` for valid commands';
+            return $this->error($msg);
+        }
     }
 
     /**
@@ -53,6 +89,25 @@ class OrderController {
             "attachments" => [
                 [
                     "text" => $help
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * Returns an error response
+     *
+     * @param string $msg
+     * @return false|string
+     */
+    private function error($msg = "There was a problem. Please try again or check `/brew help`")
+    {
+        return json_encode([
+            'response_type' => "in_channel",
+            "text" => "BrewMe Awesome Documentation",
+            "attachments" => [
+                [
+                    "text" => $msg
                 ]
             ]
         ]);
