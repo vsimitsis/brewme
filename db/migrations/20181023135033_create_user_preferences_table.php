2aae669ad4250479bf745ecc5f4679a7691b6032
Michal Nagielski <michal.nagielski@gmail.com>
2018-10-23 16:13:08 +0100

    Fixed migrations issues

--- db/migrations/20181023135033_create_user_preferences_table.php
+++ db/migrations/20181023135033_create_user_preferences_table.php
@@ -8,11 +8,11 @@ class CreateUserPreferencesTable extends AbstractMigration
     public function change()
     {
         $this->table('user_preferences', ['engine' => 'InnoDB', 'collate' => 'utf8mb4_unicode_ci', 'charset' => 'utf8mb4'])
-            ->addColumn('user_id',           'integer',   ['limit' => MysqlAdapter::INT_REGULAR, 'null' => false, 'signed' => false])
+            ->addColumn('user_id',           'integer',   ['limit' => MysqlAdapter::INT_REGULAR, 'null' => false])
             ->addColumn('type',              'string',   ['limit' => 25, 'null' => false])
             ->addColumn('comments',          'string',   ['limit' => 255, 'null' => false])
             ->addIndex(['user_id', 'type'], ['unique' => true, 'name' => 'idx_user_id_type'])
             ->addForeignKey('user_id', 'users', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
-            ->save(); 
+            ->save();
     }
 }
--- db/migrations/20181023135123_create_orders_table.php
+++ db/migrations/20181023135123_create_orders_table.php
@@ -8,7 +8,7 @@ class CreateOrdersTable extends AbstractMigration
     public function change()
     {
         $this->table('orders', ['engine' => 'InnoDB', 'collate' => 'utf8mb4_unicode_ci', 'charset' => 'utf8mb4'])
-            ->addColumn('user_id',           'integer',  ['limit' => MysqlAdapter::INT_REGULAR, 'null' => false, 'signed' => false])
+            ->addColumn('user_id',           'integer',  ['limit' => MysqlAdapter::INT_REGULAR, 'null' => false])
             ->addColumn('type',              'string',   ['limit' => 25, 'null' => false])
             ->addColumn('comments',          'string',   ['limit' => 255, 'null' => false])
             ->addColumn('status',            'integer',  ['limit' => MysqlAdapter::INT_TINY, 'null' => false, 'signed' => false])
